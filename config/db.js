fda304b20c5034ff15db96349031b623a57d3fc6
Aashrith Murari <156699951+Aashrith2004@users.noreply.github.com>
2024-11-02 21:04:36 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -6,8 +6,8 @@ const connectdb = async ()=>{
         const conn = await mongoose.connect(process.env.MONGO_LOCAL_URL)
         console.log(`DB connected ${mongoose.connection.host}`.bgMagenta.white)
     }
-    catch{
-        console.log(`Error in DB connection ${error}`.bgCyan.white)
+    catch(error){
+        console.log(`Error in DB connection ${error.message}`.bgCyan.white)
     }
 }
 
